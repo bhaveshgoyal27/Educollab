bbbc08f7366e6f929d4afae4e06353475364b55a
BHAVESH GOYAL <27goyalbhavesh@gmail.com>
2025-12-01 00:52:24 +0000

    changes

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get update 
+sudo apt-get install -y \
+    curl \
+    git \
+    unzip \
+    vim \
+    wget \
+    gcc \
+
+pip install -r requirements.txt
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "awscliv2.zip" \
+    && unzip -o awscliv2.zip \
+    && sudo ./aws/install --update \
+    && echo 'complete -C '/usr/local/bin/aws_completer' aws' >> ~/.bashrc \
+    && rm -rf awscliv2.zip ./aws
+
